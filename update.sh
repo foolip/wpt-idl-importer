3c3654e65b5e6efa56f3c511b629cdd1deb098c1
Philip Jägenstedt <philip@foolip.org>
2018-08-07 14:11:21 +0200

    Clone the two repos and copy files over

--- update.sh
+++ update.sh
@@ -3,4 +3,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo OK
+git clone --depth 1 https://github.com/web-platform-tests/wpt.git
+git clone --depth 1 https://github.com/tidoust/reffy-reports.git
+rm wpt/interfaces/*.idl
+cp reffy-reports/whatwg/idl/*.idl wpt/interfaces/
+cd wpt
+git status
