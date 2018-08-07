774b8d513f1e23ffb50341e5e822311d3452ed59
Philip Jägenstedt <philip@foolip.org>
2018-08-08 00:19:54 +0200

    Tinker with commit message

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ git clone --single-branch https://github.com/tidoust/reffy-reports.git
 # Apply fixes by merging in the wpt-idl-fixups branch.
 # Used if the reffy-reports output is temporarily broken.
 cd reffy-reports
+reffy_sha=`git rev-parse HEAD`
 git pull https://github.com/foolip/reffy-reports.git wpt-idl-fixups
 cd ..
 
@@ -30,7 +31,11 @@ cat "$pathfile" | while read path; do
     branchname="idl-file-updates-$shortname"
     git checkout -b $branchname origin/master
     git add "$path"
-    git commit -m "Update $path" -m https://github.com/foolip/wpt-idl-importer
+    git commit -F - << EOM
+Update $path
+Copied by https://github.com/foolip/wpt-idl-importer from:
+https://github.com/tidoust/reffy-reports/blob/$reffy_sha/whatwg/idl/$shortname.idl
+EOM
     git push -f fork $branchname
 done
 rm "$pathfile"
